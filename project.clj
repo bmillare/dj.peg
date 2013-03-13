5d44203cb437beff5c4bd9533b46a91245043659
Brent Millare <brent.millare@gmail.com>
2013-03-13 17:26:28 -0400

    clojars friendly name

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject dj.peg "0.3.0"
+(defproject org.bmillare/dj.peg "0.3.0"
   :description "A simple parsing expression grammar defined parser generator"
   :url "https://github.com/bmillare/dj.peg")
