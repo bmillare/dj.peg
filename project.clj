b7cf6fd45d8d4094a4b8544d7b07ee710ea2a5a4
Brent Millare <brent.millare@gmail.com>
2014-01-08 17:02:39 -0500

    version bump

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject org.bmillare/dj.peg "0.3.0"
+(defproject org.bmillare/dj.peg "0.3.1"
   :description "A simple parsing expression grammar defined parser generator"
   :url "https://github.com/bmillare/dj.peg")
