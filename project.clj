27f1761f83364bc4621ba59ef67abc1e795a9501
Brent Millare <brent.millare@gmail.com>
2014-02-18 19:27:57 -0500

    license

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject org.bmillare/dj.peg "0.3.1"
   :description "A simple parsing expression grammar defined parser generator"
-  :url "https://github.com/bmillare/dj.peg")
+  :url "https://github.com/bmillare/dj.peg"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"})
